218671fae78e311b7edc8ad1fdf001328543692a
Sushant Gupta <sushantg339@gmail.com>
2026-01-02 21:51:45 +0530

    catch all routes fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')))
 app.use('/api/contact', contactRoutes)
 
 
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
 })
 
